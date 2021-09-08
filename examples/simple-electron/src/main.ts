6ed0683c9cfd159384f6f51d7b949cc2383690ad
Richard Anaya <richard@formant.io>
2021-09-08 00:42:32 -0700

    adding login

--- examples/simple-electron/src/main.ts
+++ examples/simple-electron/src/main.ts
@@ -1,18 +1,22 @@
 import { DataManager } from "@formant/data-manager";
 import "../../../css/formant.css";
 
-document.addEventListener("load", async () => {
+document.querySelector("button")?.addEventListener("click", async () => {
+  debugger;
   const el = document.querySelector("#app");
-  if (el) {
+  const usernameField = document.querySelector("#username") as HTMLInputElement;
+  const passwordField = document.querySelector("#password") as HTMLInputElement;
+  if (el && usernameField && passwordField) {
     el.innerHTML = "Connecting";
-    if (await DataManager.waitTilAuthenticated()) {
+    try {
+      await DataManager.login(usernameField.value, passwordField.value);
       const devices = await DataManager.getDevices();
       el.innerHTML =
         DataManager.getCurrentUser()?.firstName +
         " is Authenticated, here's a list of known robots:<br/>" +
         devices.map((_) => _.name).join("<br>");
-    } else {
-      el.innerHTML = "Not Authenticated";
+    } catch (e: any) {
+      el.innerHTML = e.toString();
     }
   }
 });
