b94136d738e385d7ca3f0c633b4c888b749ce5bd
Richard Anaya <richard@formant.io>
2021-09-20 15:04:55 -0700

    fix

--- examples/teleop/src/main.ts
+++ examples/teleop/src/main.ts
@@ -1,9 +1,11 @@
 import { Authentication, Fleet } from "@formant/data-sdk";
+
 import "../../../css/formant.css";
 import "./style.css";
 import { definedAndNotNull } from "../../../packages/common/defined";
 import "../../../packages/ui-sdk-joystick/src/main";
 import "../../../packages/ui-sdk-realtime-player/src/main";
+import { Authentication, Fleet } from "@formant/data-sdk";
 
 const elBtnConnect = definedAndNotNull(
   document.querySelector("button")
@@ -15,18 +17,38 @@ const elPlayer = definedAndNotNull(
 const elJoystick = definedAndNotNull(
   document.querySelector("formant-joystick")
 ) as HTMLElement;
-const elForm = definedAndNotNull(document.querySelector("form")) as HTMLElement;
+const elSection = definedAndNotNull(
+  document.querySelector("section")
+) as HTMLElement;
 
 function log(msg: string) {
   elLog.innerHTML = msg + "<br>" + elLog.innerHTML;
 }
 
 elBtnConnect.addEventListener("click", async () => {
-  elForm.style.display = "none";
+  // hide intro
+  elSection.style.display = "none";
   elLog.style.display = "block";
+
+  // start connecting
+  log("Waiting for authentication ...");
+  if (!(await Authentication.waitTilAuthenticated())) {
+    log("Not Authenticated");
+    return;
+  }
+
+  const device = await Fleet.getCurrentDevice();
+  debugger;
+  log("Currently looking at <b>" + device.name + "</b>");
+  log("Getting a realtime connection ... ");
+  await device.startRealtimeConnection();
+  log("Connected");
+
+  // show the player
   elPlayer.style.display = "block";
+
+  // show joysticks and connec them up
   elJoystick.style.display = "block";
-  log("Connecting ...");
   elJoystick.addEventListener("joystick", (e) => {
     log(JSON.stringify(e.detail));
   });
