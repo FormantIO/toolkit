b5ebdad149639e20710166e5432d09021d360b00
Richard Anaya <richard@formant.io>
2021-09-21 10:26:25 -0700

    dont publish common

--- examples/teleop/src/main.ts
+++ examples/teleop/src/main.ts
@@ -2,23 +2,16 @@ import { Authentication, Fleet } from "@formant/data-sdk";
 
 import "../../../css/formant.css";
 import "./style.css";
-import { definedAndNotNull } from "../../../packages/common/defined";
 import "../../../packages/ui-sdk-joystick/src/main";
 import "../../../packages/ui-sdk-realtime-player/src/main";
 
-const elBtnConnect = definedAndNotNull(
-  document.querySelector("button")
+const elBtnConnect = document.querySelector("button") as HTMLElement;
+const elLog = document.querySelector("#log") as HTMLElement;
+const elPlayer = document.querySelector(
+  "formant-realtime-player"
 ) as HTMLElement;
-const elLog = definedAndNotNull(document.querySelector("#log")) as HTMLElement;
-const elPlayer = definedAndNotNull(
-  document.querySelector("formant-realtime-player")
-) as HTMLElement;
-const elJoystick = definedAndNotNull(
-  document.querySelector("formant-joystick")
-) as HTMLElement;
-const elSection = definedAndNotNull(
-  document.querySelector("section")
-) as HTMLElement;
+const elJoystick = document.querySelector("formant-joystick") as HTMLElement;
+const elSection = document.querySelector("section") as HTMLElement;
 
 function log(msg: string) {
   elLog.innerHTML = msg + "<br>" + elLog.innerHTML;
