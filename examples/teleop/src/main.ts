4c3a0ffe41ae6f4c1b6948f22bf952f935336a76
Richard Anaya <richard@formant.io>
2021-09-22 09:47:00 -0700

    using latest api to get and start video

--- examples/teleop/src/main.ts
+++ examples/teleop/src/main.ts
@@ -1,24 +1,14 @@
 import { Authentication, Fleet } from "@formant/data-sdk";
 import "@formant/ui-sdk-joystick";
 import "@formant/ui-sdk-realtime-player";
+import { RealtimePlayer } from "@formant/ui-sdk-realtime-player";
 import "./style.css";
 
-const elBtnConnect = document.querySelector("button") as HTMLElement;
-const elLog = document.querySelector("#log") as HTMLElement;
-const elPlayer = document.querySelector(
-  "formant-realtime-player"
-) as HTMLElement;
-const elJoystick = document.querySelector("formant-joystick") as HTMLElement;
-const elSection = document.querySelector("section") as HTMLElement;
-
-function log(msg: string) {
-  elLog.innerHTML = msg + "<br>" + elLog.innerHTML;
-}
-
-elBtnConnect.addEventListener("click", async () => {
+// When the user clicks "connect"
+el("button").addEventListener("click", async () => {
   // hide intro
-  elSection.style.display = "none";
-  elLog.style.display = "block";
+  el("section").style.display = "none";
+  el("#log").style.display = "block";
 
   // start connecting
   log("Waiting for authentication ...");
@@ -27,36 +17,43 @@ elBtnConnect.addEventListener("click", async () => {
     return;
   }
 
+  // get current device from url context
   const device = await Fleet.getCurrentDevice();
 
+  // start connecting to realtime and get videos and start one
   log("Currently looking at <b>" + device.name + "</b>");
   log("Getting a realtime connection ... ");
   await device.startRealtimeConnection();
   log("Connected");
 
-  const document = (await device.getCurrentConfiguration()) as any;
-  const videoStream = document.teleop.hardwareStreams[0].name as string;
-  device.addRealtimeListener((_peerId, message) => {
+  device.addRealtimeListener((_peerId: any, message: any) => {
     debugger;
-    (elPlayer as any).drawer.h264BytestreamCanvasDrawer.receiveEncodedFrame(
-      message.payload.h264VideoFrame
-    );
+    (
+      el("formant-realtime-player") as RealtimePlayer
+    ).drawer.receiveEncodedFrame(message.payload.h264VideoFrame);
   });
-  device.rtcClient?.controlRemoteStream(device.id, {
-    streamName: videoStream,
-    enable: true,
-    pipeline: "rtc",
-  });
+  let videoStreams = await device.getRealtimeVideoStreams();
+  log("Video streams: " + JSON.stringify(videoStreams));
+  log("Starting to listen to video stream " + videoStreams[0].name + " ... ");
+  device.startListeningToRealtimeVideo(videoStreams[0]);
 
   // show the player
-  elPlayer.style.display = "block";
+  el("formant-realtime-player").style.display = "block";
 
   // show joysticks and connec them up
-  elJoystick.style.display = "block";
-  elJoystick.addEventListener("joystick", (e) => {
+  el("formant-joystick").style.display = "block";
+  el("formant-joystick").addEventListener("joystick", (e) => {
     const ce = e as CustomEvent;
     log(JSON.stringify(ce.detail));
   });
 });
+
+function log(msg: string) {
+  el("#log").innerHTML = msg + "<br>" + el("#log").innerHTML;
+}
+
+function el(selector: string) {
+  return document.querySelector(selector) as HTMLElement;
+}
 
 document.body.style.display = "block";
