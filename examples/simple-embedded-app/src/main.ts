aa4b483d65fc6d7963aa2117ccf1ed30294c33f7
Richard Anaya <richard@formant.io>
2021-09-27 13:54:51 -0700

    get current data point

--- examples/simple-embedded-app/src/main.ts
+++ examples/simple-embedded-app/src/main.ts
@@ -5,11 +5,8 @@ const el = document.querySelector("#app");
 if (el) {
   el.innerHTML = "Connecting";
   if (await Authentication.waitTilAuthenticated()) {
-    const devices = await Fleet.getDevices();
-    el.innerHTML =
-      Authentication.getCurrentUser()?.firstName +
-      " is Authenticated, here's a list of known robots:<br/>" +
-      devices.map((_) => _.name).join("<br>");
+    const device = await Fleet.getCurrentDevice();
+    el.innerHTML = JSON.stringify(await device.getLatestTelemetry());
   } else {
     el.innerHTML = "Not Authenticated";
   }
