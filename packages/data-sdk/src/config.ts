9b44cbf57abfc814533189a1ab216f43788287b3
Richard Anaya <richard@formant.io>
2021-09-29 15:26:58 -0700

    latest

--- packages/data-sdk/src/config.ts
+++ packages/data-sdk/src/config.ts
@@ -1,2 +1,5 @@
-export const FORMANT_API_URL =
-  (window as any).FORMANT_API_URL || "https://api.formant.io";
+export let FORMANT_API_URL = "https://api.formant.io";
+
+if (window) {
+  FORMANT_API_URL = (window as any).FORMANT_API_URL || FORMANT_API_URL;
+}
--- packages/three-formant-urdf/src/DeviceURDF.ts
+++ packages/three-formant-urdf/src/DeviceURDF.ts
@@ -1,11 +1,13 @@
 import * as THREE from "three";
-import { getDefaultContext } from "./DeviceContext";
+import { Device, RealtimeJointStateStream } from "@formant/data-sdk";
+import { defined } from "../../common/defined";
 import { LoadingManager } from "three";
 import URDFLoader from "urdf-loader";
 import { Object3D } from "three";
 
 export class DeviceURDF extends THREE.Object3D {
-  constructor(_context = getDefaultContext()) {
+  jointStateStream: RealtimeJointStateStream | undefined;
+  constructor(private device: Device, jointStateStreamName?: string) {
     super();
     const manager = new LoadingManager();
     const loader = new URDFLoader(manager);
@@ -14,6 +16,35 @@ export class DeviceURDF extends THREE.Object3D {
       (robot) => {
         this.add(robot as unknown as Object3D);
       }
+    );
+    (async () => {
+      const joinStateStreams = await this.device.getRealtimeVideoStreams();
+      if (!jointStateStreamName) {
+        if (joinStateStreams.length === 0) {
+          throw new Error(`Device ${device?.name} has no joint state streams`);
+        }
+        this.jointStateStream = joinStateStreams[0];
+      } else {
+        this.jointStateStream = joinStateStreams.find(
+          (_) => _.name === jointStateStreamName
+        );
+        if (!this.jointStateStream) {
+          throw new Error(
+            `Device ${device?.name} has no joint state stream ${jointStateStreamName}`
+          );
+        }
+      }
+      await this._start();
+    })();
+  }
+
+  async _start() {
+    defined(this.device).addRealtimeListener((_peer, message) => {
+      if (message.header.stream.streamName === this.jointStateStream?.name) {
+      }
+    });
+    await defined(this.device).startListeningToRealtimeJointState(
+      defined(this.jointStateStream)
     );
   }
 }
