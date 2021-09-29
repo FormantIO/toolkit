428135a9e5a08187a05629baa388478c96830d9b
Richard Anaya <richard@formant.io>
2021-09-29 12:24:33 -0700

    adding threejs video element

---
+++ packages/three-formant-video/demo/main.ts
@@ -0,0 +1,60 @@
+import * as THREE from "three";
+import { Authentication, Fleet } from "@formant/data-sdk";
+import { DeviceVideo } from "../src/DeviceVideo";
+
+await Authentication.waitTilAuthenticated();
+const device = await Fleet.getCurrentDevice();
+await device.startRealtimeConnection();
+
+const scene = new THREE.Scene();
+
+const videoStreams = await device.getRealtimeVideoStreams();
+
+// Add device URDF using the current device context
+videoStreams.forEach((_, i) => {
+  const v = new DeviceVideo(_.name, device);
+  v.scale.set(2, 2, 2);
+  v.position.setX(i * 3);
+  v.rotateX(90);
+  scene.add(v);
+});
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 5;
+
+/// BOILERPLATE BELOW
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.setScalar(1024);
+directionalLight.position.set(5, 30, 5);
+scene.add(directionalLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambientLight);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const animate = function () {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+animate();
+
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
---
+++ packages/three-formant-video/src/DeviceVideo.ts
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+import { Device, Fleet, RealtimeVideoStream } from "@formant/data-sdk";
+import { defined } from "../../common/defined";
+import { H264BytestreamCanvasDrawer } from "@formant/ui-sdk-realtime-player-core";
+// @ts-ignore
+import RealtimePlayerWorker from "../node_modules/@formant/ui-sdk-realtime-player-core-worker/dist/ui-sdk-realtime-player-core-worker.umd?worker&inline";
+
+export class DeviceVideo extends THREE.Object3D {
+  videoStream: RealtimeVideoStream | undefined;
+  drawer: H264BytestreamCanvasDrawer;
+  canvas: HTMLCanvasElement;
+  mesh: THREE.Mesh;
+  constructor(videoStreamName?: string, private device?: Device) {
+    super();
+    this.drawer = new H264BytestreamCanvasDrawer(
+      () => new RealtimePlayerWorker(),
+      () => {},
+      () => {}
+    );
+    this.canvas = document.createElement("CANVAS") as HTMLCanvasElement;
+
+    const texture = new THREE.CanvasTexture(this.canvas);
+
+    const material = new THREE.MeshBasicMaterial({
+      map: texture,
+    });
+
+    const geometry = new THREE.BoxGeometry(1, 0, 1);
+    this.mesh = new THREE.Mesh(geometry, material);
+    this.add(this.mesh);
+
+    (async () => {
+      if (!this.device) {
+        this.device = await Fleet.getCurrentDevice();
+      }
+      const videoStreams = await this.device.getRealtimeVideoStreams();
+      if (!videoStreamName) {
+        if (videoStreams.length === 0) {
+          throw new Error(`Device ${device?.name} has no video streams`);
+        }
+        this.videoStream = videoStreams[0];
+      } else {
+        this.videoStream = videoStreams.find((_) => _.name === videoStreamName);
+        if (!this.videoStream) {
+          throw new Error(
+            `Device ${device?.name} has no video stream ${videoStreamName}`
+          );
+        }
+      }
+      await this._start();
+    })();
+  }
+  async _start() {
+    defined(this.device).addRealtimeListener((_peer, message) => {
+      if (message.header.stream.streamName === this.videoStream?.name) {
+        this.drawVideoFrame(message.payload.h264VideoFrame);
+      }
+    });
+    await defined(this.device).startListeningToRealtimeVideo(
+      defined(this.videoStream)
+    );
+
+    this.drawer.setCanvas(this.canvas);
+    this.drawer.start();
+  }
+
+  drawVideoFrame(h264Frame: any) {
+    this.drawer.receiveEncodedFrame(h264Frame);
+    this.mesh.scale.set(1, 0, this.canvas.height / this.canvas.width);
+    (this.mesh.material as any).map.needsUpdate = true;
+    (this.mesh.material as any).needsUpdate = true;
+  }
+}
