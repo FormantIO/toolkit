7690eeb028563a002b3e952970c0199b381fa441
Richard Anaya <richard@formant.io>
2021-09-28 20:44:43 -0700

    fix

---
+++ packages/ui-sdk-login/dist/types/main.d.ts
@@ -0,0 +1,6 @@
+export declare class Login extends HTMLElement {
+    static get observedAttributes(): string[];
+    constructor();
+    connectedCallback(): void;
+    attributeChangedCallback(name: string, _oldValue: string, newValue: string): void;
+}
