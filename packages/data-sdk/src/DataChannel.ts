fc6f0b1ee88c7f57872a586597ba893d3c1dbb79
Matthew Turnshek <matthewturnshek@gmail.com>
2021-11-18 17:05:47 -0500

    remove console logs

--- packages/data-sdk/src/DataChannel.ts
+++ packages/data-sdk/src/DataChannel.ts
@@ -2,97 +2,95 @@ type DataChannelStringListener = (message: string) => void;
 type DataChannelBinaryListener = (message: Uint8Array) => void;
 
 export class DataChannel {
-  ready = false;
-  listeners: DataChannelStringListener[] = [];
-  binaryListeners: DataChannelBinaryListener[] = [];
-  error: string | undefined;
-  decoder = new TextDecoder();
+    ready = false;
+    listeners: DataChannelStringListener[] = [];
+    binaryListeners: DataChannelBinaryListener[] = [];
+    error: string | undefined;
+    decoder = new TextDecoder();
 
-  constructor(private dataChannel: RTCDataChannel) {
-    this.dataChannel.binaryType = "arraybuffer";
-    this.dataChannel.onopen = () => {
-      this.ready = true;
-    };
-    this.dataChannel.onclose = () => {
-      this.ready = false;
-    };
-    this.dataChannel.onerror = (e) => {
-      console.error(e);
-      this.error = "An error occurred in DataChannel";
-    };
-      this.dataChannel.onmessage = (m: MessageEvent) => {
-      this.listeners.forEach(_ => {
-        console.log("listeners foreach")
-        const d = new Uint8Array(m.data);
-        const s = this.decoder.decode(d);
-        _(s)
-      })
-      this.binaryListeners.forEach(_ => {
-        console.log("binarylisteners foreach...")
-        _(new Uint8Array(m.data))
-      })
-    };
-  }
-
-  async waitTilReady(): Promise<boolean> {
-    if (this.ready) {
-      return true;
+    constructor(private dataChannel: RTCDataChannel) {
+        this.dataChannel.binaryType = "arraybuffer";
+        this.dataChannel.onopen = () => {
+            this.ready = true;
+        };
+        this.dataChannel.onclose = () => {
+            this.ready = false;
+        };
+        this.dataChannel.onerror = (e) => {
+            console.error(e);
+            this.error = "An error occurred in DataChannel";
+        };
+        this.dataChannel.onmessage = (m: MessageEvent) => {
+            this.listeners.forEach((_) => {
+                const d = new Uint8Array(m.data);
+                const s = this.decoder.decode(d);
+                _(s);
+            });
+            this.binaryListeners.forEach((_) => {
+                _(new Uint8Array(m.data));
+            });
+        };
     }
-    const p = new Promise<boolean>((resolve, reject) => {
-      let a = window.setInterval(() => {
+
+    async waitTilReady(): Promise<boolean> {
         if (this.ready) {
-          window.clearInterval(a);
-          resolve(true);
+            return true;
+        }
+        const p = new Promise<boolean>((resolve, reject) => {
+            let a = window.setInterval(() => {
+                if (this.ready) {
+                    window.clearInterval(a);
+                    resolve(true);
+                }
+                if (this.error) {
+                    reject(this.error);
+                }
+            }, 10);
+        });
+        return p;
+    }
+
+    send(data: string) {
+        if (!this.ready) {
+            throw new Error("Connection has been closed");
+        }
+        this.dataChannel.send(data);
+    }
+
+    sendBinary(data: Uint8Array) {
+        if (!this.ready) {
+            throw new Error("Connection has been closed");
+        }
+        this.dataChannel.send(data);
+    }
+
+    addListener(listener: DataChannelStringListener) {
+        this.listeners.push(listener);
+    }
+
+    removeListener(listener: DataChannelStringListener) {
+        const i = this.listeners.indexOf(listener);
+        if (i === -1) {
+            throw new Error("Could not find data channel listener to remove");
         }
         if (this.error) {
-          reject(this.error);
+            throw new Error(this.error);
         }
-      }, 10);
-    });
-    return p;
-  }
-
-  send(data: string) {
-    if (!this.ready) {
-      throw new Error("Connection has been closed");
+        this.listeners.splice(i, 1);
     }
-    this.dataChannel.send(data);
-  }
 
-  sendBinary(data: Uint8Array) {
-    if (!this.ready) {
-      throw new Error("Connection has been closed");
+    addBinaryListener(listener: DataChannelBinaryListener) {
+        this.binaryListeners.push(listener);
     }
-    this.dataChannel.send(data);
-  }
 
-  addListener(listener: DataChannelStringListener) {
-    this.listeners.push(listener);
-  }
-
-  removeListener(listener: DataChannelStringListener) {
-    const i = this.listeners.indexOf(listener);
-    if (i === -1) {
-      throw new Error("Could not find data channel listener to remove");
+    removeBinaryListener(listener: DataChannelBinaryListener) {
+        const i = this.binaryListeners.indexOf(listener);
+        if (i === -1) {
+            throw new Error("Could not find data channel listener to remove");
+        }
+        if (this.error) {
+            throw new Error(this.error);
+        }
+        this.binaryListeners.splice(i, 1);
     }
-    if (this.error) {
-      throw new Error(this.error);
-    }
-    this.listeners.splice(i, 1);
-  }
-
-  addBinaryListener(listener: DataChannelBinaryListener) {
-    this.binaryListeners.push(listener);
-  }
-
-  removeBinaryListener(listener: DataChannelBinaryListener) {
-    const i = this.binaryListeners.indexOf(listener);
-    if (i === -1) {
-      throw new Error("Could not find data channel listener to remove");
-    }
-    if (this.error) {
-      throw new Error(this.error);
-    }
-    this.binaryListeners.splice(i, 1);
-  }
 }
