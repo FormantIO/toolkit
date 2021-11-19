9aa1025f6657dee41a361c2bc5fd3e4b7e050ab4
Matthew Turnshek <matthewturnshek@gmail.com>
2021-11-19 16:27:58 -0500

    update the adapter for binary request example

--- examples/binary-request-response/adapter.py
+++ examples/binary-request-response/adapter.py
@@ -1,5 +1,4 @@
 import time
-import json
 
 from formant.sdk.agent.v1 import Client as FormantAgentClient
 
@@ -10,13 +9,9 @@ fclient = FormantAgentClient(ignore_unavailable=True)
 
 
 # Set up request handler for the given channel name.
-@fclient.custom_data_channel_request_handler(REQUEST_RESPONSE_CHANNEL_NAME)
+@fclient.custom_data_channel_binary_request_handler(REQUEST_RESPONSE_CHANNEL_NAME)
 def handler(request_data):
-    # Do something with request data (string)
-    print(json.loads(request_data))
-
-    # Return response data (string)
-    return json.dumps({"message": "Request acknowledged."})
+    return request_data  # echo back the request data
 
 
 while True:
