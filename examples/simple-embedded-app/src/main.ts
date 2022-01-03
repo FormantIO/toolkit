b813dfb95bb60ed0719f0761b7285501ad90e752
Richard Anaya <richard@formant.io>
2022-01-03 15:26:04 -0800

    bump

--- examples/simple-embedded-app/src/main.ts
+++ examples/simple-embedded-app/src/main.ts
@@ -2,22 +2,24 @@ import { Authentication, Fleet } from "@formant/data-sdk";
 import "./style.css";
 
 async function main() {
-    const el = document.querySelector("#app");
-    if (el) {
-        el.innerHTML = "Connecting";
-        if (await Authentication.waitTilAuthenticated()) {
-            const device = await Fleet.getCurrentDevice();
-            el.innerHTML = JSON.stringify(await device.getLatestTelemetry());
-        } else {
-            el.innerHTML = "Not Authenticated";
-        }
+  const el = document.querySelector("#app");
+  if (el) {
+    el.innerHTML = "Connecting";
+
+    debugger;
+    if (await Authentication.waitTilAuthenticated()) {
+      const device = await Fleet.getCurrentDevice();
+      el.innerHTML = JSON.stringify(await device.getLatestTelemetry());
+    } else {
+      el.innerHTML = "Not Authenticated";
     }
+  }
 }
 
 (async () => {
-    try {
-        await main();
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    await main();
+  } catch (e) {
+    console.log(e);
+  }
 })();
