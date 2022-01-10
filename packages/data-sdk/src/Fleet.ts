65787ca80cc7e0c354a6fbae63e8db37915c8267
Richard Anaya <richard@formant.io>
2022-01-10 11:01:47 -0800

    adding support for files url

--- packages/data-sdk/src/Fleet.ts
+++ packages/data-sdk/src/Fleet.ts
@@ -189,4 +189,22 @@ export class Fleet {
     const telemetry = await data.json();
     return telemetry.items;
   }
+
+  static async getFileUrl(uuid: string) {
+    const data = await fetch(`${FORMANT_API_URL}/v1/admin/files/query`, {
+      method: "POST",
+      body: JSON.stringify({
+        fileIds: [uuid],
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + Authentication.token,
+      },
+    });
+    const result = await data.json();
+    if (result.fileUrls.length === 0) {
+      throw new Error("File not found");
+    }
+    return result.fileUrls[0] as string;
+  }
 }
