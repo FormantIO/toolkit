f8357e5f0893607e70a9ac7e08edf82596247e4f
Richard Anaya <richard@formant.io>
2022-01-20 12:45:51 -0800

    support formant_dev

--- packages/data-sdk/src/config.ts
+++ packages/data-sdk/src/config.ts
@@ -3,3 +3,14 @@ export let FORMANT_API_URL = "https://api.formant.io";
 if (typeof window !== "undefined") {
   FORMANT_API_URL = (window as any).FORMANT_API_URL || FORMANT_API_URL;
 }
+
+let urlParams = new URLSearchParams("");
+
+if (typeof window !== "undefined") {
+  urlParams = new URLSearchParams(window.location.search);
+}
+
+const moduleName = urlParams.get("formant_dev");
+if (moduleName) {
+  FORMANT_API_URL = "https://api-dev.formant.io";
+}
