5942b0cfae6fb99f7318a57a19f613f8cf1ffbb5
alenjdev <alen@formant.io>
2022-02-04 16:31:22 -0600

    command tool ui init

---
+++ examples/ros-diagnostics/command-line/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Terminal } from "./components/Terminal/Terminal";
+
+function App() {
+  return (
+    <div className="App">
+      <Terminal />
+    </div>
+  );
+}
+
+export default App;
