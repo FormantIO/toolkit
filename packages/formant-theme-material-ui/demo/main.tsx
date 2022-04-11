550a24908a44955a468eca871365621de5a206d0
Richard Anaya <richard@formant.io>
2022-04-11 12:23:42 -0700

    upgrade

--- packages/formant-theme-material-ui/demo/main.tsx
+++ packages/formant-theme-material-ui/demo/main.tsx
@@ -10,15 +10,17 @@ import {
   createTheme,
   Grid,
   IconButton,
+  TextField,
   Toolbar,
   Typography,
 } from "@mui/material";
 import { createRoot } from "react-dom/client";
-import { lightTheme } from "../src/main";
+import { defaultTheme } from "../src/main";
 import MenuIcon from "@mui/icons-material/Menu";
 import formantLogo from "./formant.svg";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const theme = createTheme(lightTheme);
+const theme = createTheme(defaultTheme);
 
 function App() {
   return (
@@ -44,7 +46,12 @@ function App() {
             Typography
           </Typography>
           <Box sx={{ p: 2 }}>
-            <Grid container justifyContent="center" alignItems="center">
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+              rowGap={2}
+            >
               <Grid item xs={4}>
                 <Typography variant="h4">H1</Typography>
               </Grid>
@@ -105,9 +112,7 @@ function App() {
                 <Typography variant="h4">body2</Typography>
               </Grid>
               <Grid item xs={8}>
-                <Typography variant="body2">
-                  The quick brown fox jumps over the lazy dog.
-                </Typography>
+                <Typography variant="body2">0123456789</Typography>
               </Grid>
               <Grid item xs={4}>
                 <Typography variant="h4">caption</Typography>
@@ -124,15 +129,225 @@ function App() {
           </Typography>
           <Box sx={{ p: 2 }}>
             <Grid container gap={2}>
+              <Button variant="text" size="small">
+                Text
+              </Button>
+              <Button variant="contained" size="small">
+                Contained
+              </Button>
+              <Button variant="contained" size="small" color="secondary">
+                Contained
+              </Button>
+              <Button
+                variant="contained"
+                size="small"
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
+                Contained
+              </Button>
+              <Button variant="outlined" size="small">
+                Outlined
+              </Button>
+            </Grid>
+            <br />
+            <Grid container gap={2}>
               <Button variant="text">Text</Button>
               <Button variant="contained">Contained</Button>
               <Button variant="contained" color="secondary">
                 Contained
               </Button>
-              <Button variant="contained" color="error">
+              <Button
+                variant="contained"
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
                 Contained
               </Button>
               <Button variant="outlined">Outlined</Button>
+            </Grid>
+            <br />
+            <Grid container gap={2}>
+              <Button variant="text" size="large">
+                Text
+              </Button>
+              <Button variant="contained" size="large">
+                Contained
+              </Button>
+              <Button variant="contained" size="large" color="secondary">
+                Contained
+              </Button>
+              <Button
+                variant="contained"
+                size="large"
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
+                Contained
+              </Button>
+              <Button variant="outlined" size="large">
+                Outlined
+              </Button>
+            </Grid>
+          </Box>
+          <Typography variant="h2" sx={{ textDecoration: "underline" }}>
+            Text Fields
+          </Typography>
+          <Box sx={{ p: 2 }}>
+            <Grid container gap={2}>
+              <TextField
+                required
+                id="outlined-required"
+                label="Required"
+                defaultValue="Hello World"
+              />
+              <TextField
+                disabled
+                id="outlined-disabled"
+                label="Disabled"
+                defaultValue="Hello World"
+              />
+              <TextField
+                id="outlined-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+              />
+              <TextField
+                id="outlined-read-only-input"
+                label="Read Only"
+                defaultValue="Hello World"
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                id="outlined-number"
+                label="Number"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id="outlined-search"
+                label="Search field"
+                type="search"
+              />
+              <TextField
+                id="outlined-helperText"
+                label="Helper text"
+                defaultValue="Default Value"
+                helperText="Some important text"
+              />
+            </Grid>
+            <Grid container gap={2}>
+              <TextField
+                required
+                id="filled-required"
+                label="Required"
+                defaultValue="Hello World"
+                variant="filled"
+              />
+              <TextField
+                disabled
+                id="filled-disabled"
+                label="Disabled"
+                defaultValue="Hello World"
+                variant="filled"
+              />
+              <TextField
+                id="filled-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="filled"
+              />
+              <TextField
+                id="filled-read-only-input"
+                label="Read Only"
+                defaultValue="Hello World"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="filled"
+              />
+              <TextField
+                id="filled-number"
+                label="Number"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="filled"
+              />
+              <TextField
+                id="filled-search"
+                label="Search field"
+                type="search"
+                variant="filled"
+              />
+              <TextField
+                id="filled-helperText"
+                label="Helper text"
+                defaultValue="Default Value"
+                helperText="Some important text"
+                variant="filled"
+              />
+            </Grid>
+            <Grid container gap={2}>
+              <TextField
+                required
+                id="standard-required"
+                label="Required"
+                defaultValue="Hello World"
+                variant="standard"
+              />
+              <TextField
+                disabled
+                id="standard-disabled"
+                label="Disabled"
+                defaultValue="Hello World"
+                variant="standard"
+              />
+              <TextField
+                id="standard-password-input"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="standard"
+              />
+              <TextField
+                id="standard-read-only-input"
+                label="Read Only"
+                defaultValue="Hello World"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+              <TextField
+                id="standard-number"
+                label="Number"
+                type="number"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                variant="standard"
+              />
+              <TextField
+                id="standard-search"
+                label="Search field"
+                type="search"
+                variant="standard"
+              />
+              <TextField
+                id="standard-helperText"
+                label="Helper text"
+                defaultValue="Default Value"
+                helperText="Some important text"
+                variant="standard"
+              />
             </Grid>
           </Box>
           <Grid sx={{ pt: 4, pb: 4 }}>
