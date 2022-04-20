3c758cbd8615097ada6cec5b045ade914f41d80b
Richard Anaya <richard@formant.io>
2022-04-19 17:11:57 -0700

    fix

--- packages/universe/src/main.ts
+++ packages/universe/src/main.ts
@@ -1,6 +1,6 @@
-export { Universe } from "./Universe";
+export * from "./Universe";
 export * from "./IUniverseData";
-export { LayerRegistry } from "./layers/LayerRegistry";
-export { UniverseLayerContent } from "./layers/UniverseLayerContent";
-export { TransformLayer } from "./layers/TransformLayer";
-export { UniverseSidebar } from "./sidebar";
+export * from "./layers/LayerRegistry";
+export * from "./layers/UniverseLayerContent";
+export * from "./layers/TransformLayer";
+export * from "./sidebar";
