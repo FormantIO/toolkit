71165c52aca365c3d8b6a4b327b9811022ffb913
Richard Anaya <richard@formant.io>
2022-04-26 20:43:35 -0700

    push

--- packages/ui-sdk/src/Select.tsx
+++ packages/ui-sdk/src/Select.tsx
@@ -2,23 +2,27 @@ import {
   FormControl,
   InputLabel,
   Select as MuiSelect,
+  SelectChangeEvent,
   SxProps,
   Theme,
 } from "@mui/material";
 import React from "react";
-
 export interface ISelectProps {
   label: string;
   children: React.ReactNode;
   sx?: SxProps<Theme>;
   value?: any;
+  onChange?: (value: any) => void;
 }
 
 export function Select(props: ISelectProps) {
+  const onDidChange = (event: SelectChangeEvent<any>) => {
+    if (props.onChange) props.onChange(event.target.value);
+  };
   return (
     <FormControl sx={props.sx || { width: "100%" }}>
       <InputLabel>{props.label}</InputLabel>
-      <MuiSelect value={props.value} label={props.label}>
+      <MuiSelect value={props.value} label={props.label} onChange={onDidChange}>
         {props.children}
       </MuiSelect>
     </FormControl>
