2cdc5b60a3b73899c81e78d1afb5a6eb17b046a2
Richard Anaya <richard@formant.io>
2022-05-01 16:36:58 -0700

    fixes

--- packages/universe/demo/SimulatedUniverseData.ts
+++ packages/universe/demo/SimulatedUniverseData.ts
@@ -14,17 +14,20 @@ import {
 import { IRtcPointCloud } from "../../data-sdk/src/model/IRtcPointCloud";
 
 export class SimulatedUniverseData implements IUniverseData {
-  async getLatestTransformTrees(): Promise<
-    { streamName: string; transformTree: ITransformNode }[]
-  > {
-    return [
-      {
-        streamName: "spotTf",
-        transformTree: await fetch(
-          "https://upload.formant.io/0d29f656-cc1c-4b9e-baad-199cfa1fcced/b0990d5a-cdff-4c3c-ab71-c6c72be385ad/2022/05/1/af202124-bab3-4191-8948-458dc96efaef"
-        ).then((_) => _.json() as ITransformNode),
-      },
-    ];
+  async getLatestTransformTrees(
+    deviceId: string
+  ): Promise<{ streamName: string; transformTree: ITransformNode }[]> {
+    if (deviceId === "abc") {
+      return [
+        {
+          streamName: "spotTf",
+          transformTree: await fetch(
+            "https://formant-3d-models.s3.us-west-2.amazonaws.com/spotjoint_sit.json"
+          ).then((_) => _.json() as ITransformNode),
+        },
+      ];
+    }
+    return [];
   }
 
   async getLatestLocations(): Promise<
@@ -169,10 +172,11 @@ export class SimulatedUniverseData implements IUniverseData {
     ];
   }
 
-  async getUrdfs(_deviceId: string): Promise<string[]> {
-    return [
-      "https://telemetry-prod-usw2-file-storage-service.s3.amazonaws.com/files/0d29f656-cc1c-4b9e-baad-199cfa1fcced/ff2b4e4f-80fa-4f30-b21a-b51b3538c4cc?AWSAccessKeyId=ASIA3QXDC4OAOOW32EXQ&Expires=1651461837&Signature=aOeBbkHKasFwPk%2Bp0K%2FYrB7bkqo%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEFsaCXVzLXdlc3QtMiJGMEQCIF%2FE3FSpZ1wMA6zKrWs8PotRY%2BRPDdI3I%2FoZ0rzZVfYYAiBnEMX6K3nPimcBpBs2LIIzzAMlKd6wib3JmPGFFDae6yrSBAgkEAMaDDc5MTgyMzk2NzEwNCIMkpS4qnuAlN2QLMerKq8E0yvznfYrH1eRHQH2efHo53Yp8KtnjMWjfhcrclErok%2FWuencR4nnErBcUGJJ4QhCVoaDi4%2Bl0CPcCBHgczZSM6sJuATlcdn9mGsHWc6RYvJhYR%2FSffeq%2FM0zrR2tKeUkxiempcA3DpnzTOECbRLhPHKCdIanvpOPm1iq%2Bpa9vGiI0XVHd49L57fOCSpW8XmYLfer4Xnb1RMu%2FlFcnpHmD4zMK62Rk938K891xfdIi6JPUB0ee5MntaxI6Wrw2kOQ1J9MASicEDH%2F%2BCzipj6UDZ1R%2FlMS2Y405wN8%2BYICv0oAKuMFIfHaDRQR%2BPCnSsTuvCAGXEoJcW6122CoryvPHcUE4S6yDI9RmGR1SORnDJZdZyQfDAsuWtMrUqnaXvaL5ioegz1kZWlosbuYYPJqXDjri7KMppnGDYhKT6PBvWMBo%2BkPrk7VJpakU%2BU%2Bci4dQqr8g7HQ3jvYSZNJfw%2FhhHLKpHnxFgCJ5sENS3gY5rJM73X%2F91cpbE1sMN9KalL%2FQet4oRYZNTd3Tyv7iGVL14ejBWSkjKzegXwaBLzggaG9DWtHeZxHKUx0baLpcfcLoJyc%2F7BPWpOq7bfONBm%2BkPKAump2SNVwEyONZcUmt%2BjdaNsYZATAMk0ABnxW8aKiDutvPbmZ1fOVsdB8Nu2B2%2BX3wfs66UXqEzncLmsGQgowTwvUhQsjvyLsMokVMz%2BWgA0qJuhCGEwlZYgN%2B4zsE6tVGb5ooawimNwRo37i8DCI9reTBjqqAUMuymW4ktzSAlDxPLjAEIvKyQhkrxAngpVCaTiTx7g3p%2FH9Gj6frefydguCUQ%2FhGtlKnZY56IHksJNZGYOVqve9NJOKxdLGMkVdCoekKLNGKZ9c8oA95pn3BpB6kPswQjV%2B6CV3thymsAVmG2BDRo0iMhQDbpATD3v82%2FPzPj5wOdXJ0Y0NwRBcIa%2BWib%2FCGxuFcczdHUltTttNYwM3LL7stS2JySLVY62A",
-    ];
+  async getUrdfs(deviceId: string): Promise<string[]> {
+    if (deviceId === "abc") {
+      return ["https://formant-3d-models.s3.us-west-2.amazonaws.com/spot.zip"];
+    }
+    return ["https://formant-3d-models.s3.us-west-2.amazonaws.com/arm.zip"];
   }
 
   getHardwareStreams(_deviceId: string): IHardwareStream[] {
@@ -184,7 +188,7 @@ export class SimulatedUniverseData implements IUniverseData {
     callback: (data: ITransformNode) => void
   ): () => void {
     callback({
-      url: "https://upload.formant.io/0d29f656-cc1c-4b9e-baad-199cfa1fcced/b0990d5a-cdff-4c3c-ab71-c6c72be385ad/2022/05/1/af202124-bab3-4191-8948-458dc96efaef",
+      url: "https://formant-3d-models.s3.us-west-2.amazonaws.com/spotjoint_sit.json",
     });
     return () => {};
   }
