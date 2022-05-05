38a07e6e31dc0e17d5bf256a2bcbea55fe235d4f
Richard Anaya <richard@formant.io>
2022-05-05 01:02:31 -0700

    simpler

--- examples/universe-custom-layer/src/CubeLayer.ts
+++ examples/universe-custom-layer/src/CubeLayer.ts
@@ -1,63 +1,30 @@
-import {
-  IUniverseData,
-  TransformLayer,
-  UniverseDataSource,
-  UniverseLayerContent,
-} from "@formant/universe";
+import { UniverseLayer } from "@formant/universe";
 import * as THREE from "three";
 
-export class CubeLayer extends UniverseLayerContent {
-  static id = "cube";
+export class CubeLayer extends UniverseLayer {
+  static layerTypeId = "cube";
   static commonName = "Cube";
-  static description = "A cube.";
-  static usesData = false;
+  static description = "This is just a simple cube.";
 
   cube = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({ color: 0x20a0ff })
   );
 
-  static createDefault(
-    layerId: string,
-    _universeData: IUniverseData,
-    _deviceId: string,
-    _universeDataSources?: UniverseDataSource[]
-  ): TransformLayer<CubeLayer> {
-    return new TransformLayer(layerId, new CubeLayer(layerId));
-  }
-
-  constructor(layerId?: string) {
-    super(layerId as string);
+  init() {
     this.add(this.cube);
   }
 
   onPointerMove(raycaster: THREE.Raycaster): void {
-    let intersects = false;
-    if (raycaster.intersectObject(this.cube).length > 0) {
-      intersects = true;
-    }
-    let m;
-    if (Array.isArray(this.cube.material)) {
-      m = this.cube.material[0];
-    } else {
-      m = this.cube.material;
-    }
-    if (m instanceof THREE.MeshBasicMaterial) {
-      m.color.set(intersects ? 0x20a0ff : 0xffffff);
-    }
+    let intersects = raycaster.intersectObject(this.cube).length > 0;
+    (this.cube.material as THREE.MeshBasicMaterial).color.set(
+      intersects ? 0x20a0ff : 0xffffff
+    );
   }
 
   onPointerDown(raycaster: THREE.Raycaster): void {
     if (raycaster.intersectObject(this.cube).length > 0) {
-      let m;
-      if (Array.isArray(this.cube.material)) {
-        m = this.cube.material[0];
-      } else {
-        m = this.cube.material;
-      }
-      if (m instanceof THREE.MeshBasicMaterial) {
-        m.color.set(0xff0000);
-      }
+      (this.cube.material as THREE.MeshBasicMaterial).color.set(0xff0000);
     }
   }
 
