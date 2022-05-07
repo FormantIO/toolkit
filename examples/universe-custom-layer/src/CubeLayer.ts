7759fa7c0f1e94b87bc0ee62327cf4ac62b83356
Richard Anaya <richard@formant.io>
2022-05-07 00:05:51 -0700

    simple hand test

--- examples/universe-custom-layer/src/CubeLayer.ts
+++ examples/universe-custom-layer/src/CubeLayer.ts
@@ -9,55 +9,23 @@ export class CubeLayer extends UniverseLayer {
   geo = new THREE.BoxGeometry(1, 1, 1);
   mat = new THREE.MeshBasicMaterial({ color: 0x20a0ff });
   cube = new THREE.Mesh(this.geo, this.mat);
-  usingHands = false;
 
   init() {
     this.add(this.cube);
   }
 
-  onPointerMove(raycaster: THREE.Raycaster): void {
-    if (!this.usingHands) {
-      let intersects = raycaster.intersectObject(this.cube).length > 0;
-      this.mat.color.set(intersects ? 0x20a0ff : 0xffffff);
-    }
-  }
-
-  onPointerDown(raycaster: THREE.Raycaster): void {
-    if (!this.usingHands) {
-      if (raycaster.intersectObject(this.cube).length > 0) {
-        this.mat.color.set(0xff0000);
+  onHandsMoved(hands: FormantHandModel[]): void {
+    let intersects = false;
+    hands.forEach((hand) => {
+      if (hand.intersectBoxObject(this.cube)) {
+        intersects = true;
       }
-    }
-  }
+    });
 
-  onPointerUp(_raycaster: THREE.Raycaster, _button: number): void {
-    this.showSnackbar("Clicked!");
-  }
-
-  onEnterVR(_xr: THREE.WebXRManager): void {
-    this.mat.color.set(0x00ff00);
-  }
-
-  onExitVR(_xr: THREE.WebXRManager): void {
-    this.mat.color.set(0xffffff);
-  }
-
-  onHandsEnter(_hands: FormantHandModel[]): void {
-    this.mat.color.set(0x000ff0);
-    this.usingHands = true;
-  }
-  onHandsLeave(_hands: FormantHandModel[]): void {
-    this.mat.color.set(0x0ff000);
-    this.usingHands = false;
-  }
-
-  onHandsUpdate(hands: FormantHandModel[]): void {
-    if (this.usingHands) {
-      hands.forEach((hand) => {
-        if (hand.intersectBoxObject(this.cube)) {
-          this.mat.color.set(0xf00f00);
-        }
-      });
+    if (intersects) {
+      this.mat.color.set(0xff0000);
+    } else {
+      this.mat.color.set(0x20a0ff);
     }
   }
 
