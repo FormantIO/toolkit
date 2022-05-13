4980e05c0ba901cb4b12ee9d8a0a0592956e89fd
Richard Anaya <richard@formant.io>
2022-05-12 18:12:49 -0700

    fix

--- packages/universe/src/layers/RealtimeVideoLayer.ts
+++ packages/universe/src/layers/RealtimeVideoLayer.ts
@@ -1,4 +1,11 @@
-import { BoxGeometry, CanvasTexture, Mesh, MeshBasicMaterial } from "three";
+import {
+  BackSide,
+  BoxGeometry,
+  CanvasTexture,
+  Mesh,
+  MeshBasicMaterial,
+  SphereGeometry,
+} from "three";
 import * as uuid from "uuid";
 import { H264BytestreamCanvasDrawer } from "@formant/ui-sdk-realtime-player-core";
 import { IUniverseData } from "../model/IUniverseData";
@@ -15,9 +22,20 @@ export class RealtimeVideoLayer extends UniverseLayer {
 
   static commonName = "Realtime Video";
 
-  static description = "A video plane representing a realtime camera.";
+  static description = "A video representing a realtime camera.";
 
   static usesData = true;
+
+  static fields = {
+    video_shape: {
+      name: "Shape",
+      description: "The shape you'd like the video to be",
+      placeholder: "sphere",
+      value: "",
+      type: "text",
+      location: ["create"],
+    },
+  };
 
   static async getLayerSuggestions(
     universeData: IUniverseData,
@@ -47,9 +65,9 @@ export class RealtimeVideoLayer extends UniverseLayer {
 
   loaded: boolean = false;
 
-  drawer!: H264BytestreamCanvasDrawer;
+  drawer?: H264BytestreamCanvasDrawer;
 
-  mesh!: Mesh;
+  mesh?: Mesh;
 
   init() {
     const dataSource = defined(this.layerDataSources)[0];
@@ -64,28 +82,46 @@ export class RealtimeVideoLayer extends UniverseLayer {
       this.onData
     );
     const canvas = document.createElement("CANVAS") as HTMLCanvasElement;
-
+    canvas.width = 0;
+    canvas.height = 0;
     this.drawer.setCanvas(canvas);
     this.drawer.start();
   }
 
   onData = (frame: IH264VideoFrame) => {
+    const drawer = defined(this.drawer);
+    drawer.receiveEncodedFrame(frame);
     // we lazily create this canvas because threejs doesn't like size changes
-    const canvas = defined(this.drawer.canvas);
-    const texture = new CanvasTexture(canvas);
+    const canvas = defined(drawer.canvas);
+    if (!this.mesh && canvas.width > 0 && canvas.height > 0) {
+      const texture = new CanvasTexture(canvas);
 
-    const material = new MeshBasicMaterial({
-      map: texture,
-    });
-
-    const geometry = new BoxGeometry(1, 1, 0);
-    const ninetyDegrees = Math.PI / 2;
-    this.mesh = new Mesh(geometry, material);
-    this.mesh.rotation.set(ninetyDegrees, 0, 0);
-    this.add(this.mesh);
-    this.drawer.receiveEncodedFrame(frame);
-    this.mesh.scale.set(1, canvas.height / canvas.width, 0);
-    (this.mesh.material as any).map.needsUpdate = true;
-    (this.mesh.material as any).needsUpdate = true;
+      const shapeField = (this.layerFields || {}).video_shape;
+      const shape = shapeField.value;
+      if (shape === "sphere") {
+        const material = new MeshBasicMaterial({
+          map: texture,
+          side: BackSide,
+        });
+        const geometry = new SphereGeometry(0.3);
+        const ninetyDegrees = Math.PI / 2;
+        this.mesh = new Mesh(geometry, material);
+        this.mesh.rotation.set(ninetyDegrees, 0, 0);
+      } else {
+        const material = new MeshBasicMaterial({
+          map: texture,
+        });
+        const geometry = new BoxGeometry(1, 1, 0);
+        const ninetyDegrees = Math.PI / 2;
+        this.mesh = new Mesh(geometry, material);
+        this.mesh.rotation.set(ninetyDegrees, 0, 0);
+      }
+      this.add(this.mesh);
+      if (shape !== "sphere") {
+        this.mesh.scale.set(1, canvas.height / canvas.width, 0);
+      }
+      (this.mesh.material as any).map.needsUpdate = true;
+      (this.mesh.material as any).needsUpdate = true;
+    }
   };
 }
