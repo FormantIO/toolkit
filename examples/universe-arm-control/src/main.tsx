92fc896d34920efe40d5513e959c8f2317039af8
Richard Anaya <richard@formant.io>
2022-05-21 22:36:03 -0700

    publish demo

---
+++ examples/universe-arm-control/src/main.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { FormantProvider } from "@formant/ui-sdk";
+import { LayerRegistry, TeleportLayer, Universe } from "@formant/universe";
+import { SimulatedUniverseData } from "./SimulatedUniverseData";
+import { createScene } from "./createScene";
+import { App as FormantApp, ModuleData } from "@formant/data-sdk";
+import { HandsLayer } from "./HandsLayer";
+import { XboxLayer } from "./XboxLayer";
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const mode = urlParams.get("mode");
+const vr = urlParams.get("vr");
+
+LayerRegistry.register(TeleportLayer);
+LayerRegistry.register(HandsLayer);
+LayerRegistry.register(XboxLayer);
+
+function App() {
+  const data = new SimulatedUniverseData();
+  if (FormantApp.isModule()) {
+    FormantApp.addModuleDataListener((moduleData: ModuleData) => {
+      data.setTime(moduleData.time);
+    });
+  } else {
+    window.setInterval(() => {
+      data.setTime(Date.now());
+    }, 60 / 12);
+  }
+  return (
+    <Universe
+      universeData={data}
+      mode={
+        mode === "edit" || mode === "view" || mode === "no-interaction"
+          ? mode
+          : "edit"
+      }
+      initialSceneGraph={createScene()}
+      onSceneGraphChange={(_) => {
+        console.log(JSON.stringify(_));
+      }}
+      vr={vr === "true"}
+    ></Universe>
+  );
+}
+
+const container = document.getElementById("app");
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <FormantProvider>
+      <App />
+    </FormantProvider>
+  );
+}
