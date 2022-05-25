1cd9dd03e3e36f2fdd3ebdd33bf4cb129af21d0d
Franklin <franklin@formant.io>
2022-05-25 10:19:29 -0400

    Refactored the parser to make it use classes

--- examples/ros-service-call-adapter/adapter/input_parser.py
+++ examples/ros-service-call-adapter/adapter/input_parser.py
@@ -12,6 +12,21 @@ import ast
 DELIMITERS = {"}", ")", "]", " ", "", ","}
 
 
+def parse(input: str):
+    """Parses a string for parsing data for use
+        in ros messages
+
+    The grammar:
+    Primitive = Int | Float | String | Bool
+    List = [ Primitive, ... ]
+    Tuple = ( Primitive, ... )
+    Set = { Primitive, ... }
+    """
+
+    rdp = _ROS_MSG_RDP(input)
+    return rdp.parse_input()
+
+
 class StringStream:
     """The string stream class allows the user to read a string of characters through peek_char and read_char."""
 
@@ -40,170 +55,154 @@ class StringStream:
             raise SyntaxError(f"Unexpected EOF. Cannot parse {self.input}")
 
 
-def parse(input: str):
-    """Parses a string for parsing data for use
-        in ros messages
+class _ROS_MSG_RDP:
 
-    The grammar:
-    Primitive = Int | Float | String | Bool
-    List = [ Primitive, ... ]
-    Tuple = ( Primitive, ... )
-    Set = { Primitive, ... }
-    """
-    ss = StringStream(input)
-    return parse_input(ss)
+    def __init__(self, input_str):
+        self.string_stream = StringStream(input_str)
 
+    def parse_input(self):
+        if self.string_stream.peek_char() == "[":
+            return self.parse_list()
 
-def parse_input(string_stream: StringStream):
-    if string_stream.peek_char() == "[":
-        return parse_list(string_stream)
+        if self.string_stream.peek_char() == "(":
+            return tuple(self.parse_list("(", ")"))
 
-    if string_stream.peek_char() == "(":
-        return tuple(parse_list(string_stream, "(", ")"))
+        if self.string_stream.peek_char() == "{":
+            return set(self.parse_list("{", "}"))
 
-    if string_stream.peek_char() == "{":
-        return set(parse_list(string_stream, "{", "}"))
+        if self.string_stream.peek_char().isnumeric() or self.string_stream.peek_char() == "." or self.string_stream.peek_char() == "-":
+            return self.parse_numeric()
 
-    if string_stream.peek_char().isnumeric() or string_stream.peek_char() == "." or string_stream.peek_char() == "-":
-        return parse_numeric(string_stream)
+        if self.string_stream.peek_char() == '"' or self.string_stream.peek_char() == "'":
+            delimiter = self.string_stream.peek_char()
+            return self.parse_string(delimiter)
 
-    if string_stream.peek_char() == '"' or string_stream.peek_char() == "'":
-        delimiter = string_stream.peek_char()
-        return parse_string(string_stream, delimiter)
+        if self.string_stream.peek_char() == "F" or self.string_stream.peek_char() == "T":
+            return self.parse_bool()
 
-    if string_stream.peek_char() == "F" or string_stream.peek_char() == "T":
-        return parse_bool(string_stream)
+        raise SyntaxError(f"Cannot parse input")
 
-    raise SyntaxError(f"Cannot parse input")
+    def parse_bool(self):
+        if self.string_stream.peek_char() != "F" and self.string_stream.peek_char() != "T":
+            raise SyntaxError(
+                f"Invalid boolean expression. Cannot parse {self.string_stream.input}")
 
+        if self.string_stream.peek_char() == "F":
+            return self.parse_bool_false()
 
-def parse_bool(string_stream: StringStream):
-    if string_stream.peek_char() != "F" and string_stream.peek_char() != "T":
+        if self.string_stream.peek_char() == "T":
+            return self.parse_bool_true()
+
         raise SyntaxError(
-            f"Invalid boolean expression. Cannot parse {string_stream.input}")
+            f"Invalid boolean. Cannot parse {self.string_stream.input}")
 
-    if string_stream.peek_char() == "F":
-        return parse_bool_false(string_stream)
+    def parse_bool_false(self):
+        self.parse_from_stream("False")
+        return False
 
-    if string_stream.peek_char() == "T":
-        return parse_bool_true(string_stream)
+    def parse_bool_true(self):
+        self.parse_from_stream("True")
+        return True
 
-    raise SyntaxError(f"Invalid boolean. Cannot parse {string_stream.input}")
+    def parse_from_stream(self, string_to_parse: str):
+        for character in string_to_parse:
+            if self.string_stream.read_char() != character:
+                raise SyntaxError(f"Unmatched Character. Expected {character}")
 
+        if self.string_stream.peek_char() not in DELIMITERS:
+            raise SyntaxError("Expected Delimiter")
 
-def parse_bool_false(string_stream):
-    parse_from_stream(string_stream, "False")
-    return False
+    def parse_numeric(self):
+        if not self.string_stream.peek_char().isnumeric() and not self.string_stream.peek_char() == "."\
+                and not self.string_stream.peek_char() == "-":
+            raise SyntaxError(
+                f"Invalid input. Cannot parse {self.string_stream.input}")
 
-
-def parse_bool_true(string_stream):
-    parse_from_stream(string_stream, "True")
-    return True
-
-
-def parse_from_stream(string_stream: StringStream, string_to_parse: str):
-    for character in string_to_parse:
-        if string_stream.read_char() != character:
-            raise SyntaxError(f"Unmatched Character. Expected {character}")
-
-    if string_stream.peek_char() not in DELIMITERS:
-        raise SyntaxError("Expected Delimiter")
-
-
-def parse_numeric(string_stream: StringStream):
-    if not string_stream.peek_char().isnumeric() and not string_stream.peek_char() == "."\
-            and not string_stream.peek_char() == "-":
-        raise SyntaxError(f"Invalid input. Cannot parse {string_stream.input}")
-
-    if string_stream.peek_char().isnumeric():
-        return parse_numeric_before_decimal(string_stream)
-    elif string_stream.peek_char() == "-":
-        delim = string_stream.read_char()
-        string_stream.validate_stream()
-        return parse_numeric_before_decimal(string_stream, delim)
-    else:
-        return parse_numeric_after_decimal(string_stream, string_stream.read_char())
-
-
-def parse_numeric_before_decimal(string_stream: StringStream, soFar=""):
-
-    if string_stream.peek_char().isnumeric():
-        return parse_numeric_before_decimal(string_stream, soFar+string_stream.read_char())
-
-    elif string_stream.peek_char() == ".":
-        return parse_numeric_after_decimal(string_stream, soFar+string_stream.read_char())
-
-    elif string_stream.peek_char() not in DELIMITERS:
-        raise SyntaxError(f"Expected delimiter")
-
-    return int(soFar)
-
-
-def parse_numeric_after_decimal(string_stream: StringStream, soFar=""):
-    if string_stream.peek_char().isnumeric():
-        return parse_numeric_after_decimal(string_stream, soFar+string_stream.read_char())
-    elif string_stream.peek_char() not in DELIMITERS:
-        raise SyntaxError(
-            f"Expected delimiter, got {string_stream.peek_char()}")
-    return float(soFar)
-
-
-def parse_string(string_stream: StringStream, string_delimiter='"'):
-    string = ""
-
-    if string_stream.read_char() != string_delimiter:
-        raise SyntaxError(
-            f"Expected {string_delimiter} got {string_stream.peek_char()}")
-
-    while string_stream.peek_char() != string_delimiter:
-
-        if string_stream.peek_char() == "\\":
-            string += parse_escaped(string_stream)
+        if self.string_stream.peek_char().isnumeric():
+            return self.parse_numeric_before_decimal()
+        elif self.string_stream.peek_char() == "-":
+            delim = self.string_stream.read_char()
+            self.string_stream.validate_stream()
+            return self.parse_numeric_before_decimal(delim)
         else:
-            string += string_stream.read_char()
+            return self.parse_numeric_after_decimal(self.string_stream.read_char())
 
-        string_stream.validate_stream()
+    def parse_numeric_before_decimal(self, soFar=""):
 
-    string_stream.read_char()
+        if self.string_stream.peek_char().isnumeric():
+            return self.parse_numeric_before_decimal(soFar+self.string_stream.read_char())
 
-    return string
+        elif self.string_stream.peek_char() == ".":
+            return self.parse_numeric_after_decimal(soFar+self.string_stream.read_char())
 
+        elif self.string_stream.peek_char() not in DELIMITERS:
+            raise SyntaxError(f"Expected delimiter")
 
-def parse_escaped(string_stream: StringStream):
-    if string_stream.peek_char() != "\\":
-        raise SyntaxError(
-            f"Expected \\, got {string_stream.peek_char()}. Cannot parse {string_stream.input}")
+        return int(soFar)
 
-    string_stream.read_char()
-    string_stream.validate_stream()
-    return ast.literal_eval(f"'\\{string_stream.read_char()}'")
+    def parse_numeric_after_decimal(self, soFar=""):
+        if self.string_stream.peek_char().isnumeric():
+            return self.parse_numeric_after_decimal(soFar+self.string_stream.read_char())
+        elif self.string_stream.peek_char() not in DELIMITERS:
+            raise SyntaxError(
+                f"Expected delimiter, got {self.string_stream.peek_char()}")
+        return float(soFar)
 
+    def parse_string(self, string_delimiter='"'):
+        string = ""
 
-def parse_list(string_stream: StringStream, list_start="[", list_delim="]"):
-    if string_stream.read_char() != list_start:
-        raise SyntaxError(f"Invalid List. Cannot parse {string_stream.input}")
+        if self.string_stream.read_char() != string_delimiter:
+            raise SyntaxError(
+                f"Expected {string_delimiter} got {self.string_stream.peek_char()}")
 
-    parsed_list = []
-    string_stream.remove_leading_whitespace()
+        while self.string_stream.peek_char() != string_delimiter:
 
-    if string_stream.peek_char() == ",":
-        raise SyntaxError(
-            f"Must have element before ,. Cannot parse {string_stream.input}")
+            if self.string_stream.peek_char() == "\\":
+                string += self.parse_escaped()
+            else:
+                string += self.string_stream.read_char()
 
-    while string_stream.peek_char() != list_delim:
+            self.string_stream.validate_stream()
 
-        string_stream.remove_leading_whitespace()
-        parsed_list.append(parse_input(string_stream))
-        string_stream.validate_stream()
-        string_stream.remove_leading_whitespace()
+        self.string_stream.read_char()
 
-        if string_stream.peek_char() != ',' and string_stream.peek_char() != list_delim:
-            raise SyntaxError("List must either have ] or ,")
+        return string
 
-        if string_stream.peek_char() == ",":
-            string_stream.read_char()
-            if string_stream.peek_char() == list_delim:
-                raise SyntaxError("Commas must separate elements. ")
+    def parse_escaped(self):
+        if self.string_stream.peek_char() != "\\":
+            raise SyntaxError(
+                f"Expected \\, got {self.string_stream.peek_char()}. Cannot parse {self.string_stream.input}")
 
-    string_stream.read_char()
-    return parsed_list
+        self.string_stream.read_char()
+        self.string_stream.validate_stream()
+        return ast.literal_eval(f"'\\{self.string_stream.read_char()}'")
+
+    def parse_list(self, list_start="[", list_delim="]"):
+        if self.string_stream.read_char() != list_start:
+            raise SyntaxError(
+                f"Invalid List. Cannot parse {self.string_stream.input}")
+
+        parsed_list = []
+        self.string_stream.remove_leading_whitespace()
+
+        if self.string_stream.peek_char() == ",":
+            raise SyntaxError(
+                f"Must have element before ,. Cannot parse {self.string_stream.input}")
+
+        while self.string_stream.peek_char() != list_delim:
+
+            self.string_stream.remove_leading_whitespace()
+            parsed_list.append(self.parse_input())
+            self.string_stream.validate_stream()
+            self.string_stream.remove_leading_whitespace()
+
+            if self.string_stream.peek_char() != ',' and self.string_stream.peek_char() != list_delim:
+                raise SyntaxError("List must either have ] or ,")
+
+            if self.string_stream.peek_char() == ",":
+                self.string_stream.read_char()
+                if self.string_stream.peek_char() == list_delim:
+                    raise SyntaxError("Commas must separate elements. ")
+
+        self.string_stream.read_char()
+        return parsed_list
