014431f66f614599eb8ed7b2a70fe84474e0d84c
Franklin Volcic <franklin@formant.io>
2022-07-06 13:30:57 -0400

    Updated the service call adapter start script

--- examples/ros-service-call-adapter/adapter/start.sh
+++ examples/ros-service-call-adapter/adapter/start.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-variable=$(./setup.py)
-
-n=${#variable}
-if [ $n -ne 0 ]; then
-    source $variable
-    echo "source $variable"
-fi
-
-pip3 install -r requirements.txt
-
 /usr/bin/python3 main.py
