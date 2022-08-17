4598a554c79b0e8f56d1f60c0f4a6e62bf731651
alenjdev <alen@formant.io>
2022-08-17 13:55:43 -0500

    stop adapter from running

--- examples/ros-diagnostics/adapter/start.sh
+++ examples/ros-diagnostics/adapter/start.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
-### Fix ROS install on weird agent docker deployments
-# apt-get install python3-pip
-# apt-get purge python-rospkg && apt-get update && apt-get install -y ipython python-rospkg
-# python3 -m pip install -r requirements.txt
-
 ### Source Availiable ROS
-for dir in `ls /opt/ros | sort -r`; do
-    if [ -f "/opt/ros/$dir/setup.bash" ]; then
-        echo "Sourcing /opt/ros/$dir/setup.bash."
-        . /opt/ros/$dir/setup.bash
-        break
-    fi
-done
-source config.env
-python3 main.py
+if [ -d "/opt/ross" ]; then
+    export PYTHONUNBUFFERED=true
+    pip3 install setuptools
+    pip3 install wheel
+    python3 -m pip install -r requirements.txt
+    for dir in $(ls /opt/ros | sort -r); do
+        if [ -f "/opt/ros/$dir/setup.bash" ]; then
+            echo "Sourcing /opt/ros/$dir/setup.bash."
+            . /opt/ros/$dir/setup.bash
+            break
+        fi
+    done
+    source config.env
+    python3 main.py
+fi
