a010b32408177523b19e11c169f40286e71147a3
alenjdev <alen@formant.io>
2022-09-08 16:24:03 -0500

    make file sleep for a day when ROS is not installed

--- examples/ros-diagnostics/adapter/start.sh
+++ examples/ros-diagnostics/adapter/start.sh
@@ -1,18 +1,21 @@
 #! /bin/bash
 
 ### Source Availiable ROS
-if [ -d "/opt/ross" ]; then
-    export PYTHONUNBUFFERED=true
-    pip3 install setuptools
-    pip3 install wheel
-    python3 -m pip install -r requirements.txt
-    for dir in $(ls /opt/ros | sort -r); do
-        if [ -f "/opt/ros/$dir/setup.bash" ]; then
-            echo "Sourcing /opt/ros/$dir/setup.bash."
-            . /opt/ros/$dir/setup.bash
-            break
-        fi
-    done
-    source config.env
-    python3 main.py
+if [ ! -d "/opt/ros" ]; then
+    sleep 1d
 fi
+
+export PYTHONUNBUFFERED=true
+pip3 install setuptools
+pip3 install wheel
+python3 -m pip install -r requirements.txt
+for dir in $(ls /opt/ros | sort -r); do
+    if [ -f "/opt/ros/$dir/setup.bash" ]; then
+        echo "Sourcing /opt/ros/$dir/setup.bash."
+        . /opt/ros/$dir/setup.bash
+        break
+    fi
+done
+source config.env
+export PYTHONPATH=/usr/lib/formant/agent/dist-packages/:$PYTHONPATH
+python3 main.py
