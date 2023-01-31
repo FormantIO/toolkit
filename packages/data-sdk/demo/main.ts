0af0a1b14e5992dac8e821538edb298da46c89ad
Richard Anaya <richard@formant.io>
2023-01-30 16:32:29 -0800

    marker fi

--- packages/data-sdk/demo/main.ts
+++ packages/data-sdk/demo/main.ts
@@ -1,16 +1,16 @@
-import { Authentication, Fleet } from "../src/main";
+import { Authentication } from "../src/main";
 import "./style.css";
 
 (async function () {
   const el = document.querySelector("#app");
   if (el) {
     if (await Authentication.waitTilAuthenticated()) {
-      const events = await Fleet.queryEvents({});
+      /*const events = await Fleet.queryEvents({});
       const data = await Fleet.queryTelemetry({
         start: new Date(Date.now() - 1000 * 60).toISOString(),
         end: new Date().toISOString(),
         deviceIds: Fleet.defaultDeviceId ? [Fleet.defaultDeviceId] : [],
-      });
+      });*/
     }
   }
 })();
--- packages/data-sdk/src/model/IMarker3D.ts
+++ packages/data-sdk/src/model/IMarker3D.ts
@@ -1,24 +1,29 @@
 import { IColorRGBA } from "./IColorRGBA";
+import { IQuaternion } from "./IQuaternion";
 import { ITransform } from "./ITransform";
 import { IVector3 } from "./IVector3";
-import { Marker3DAction } from "./Marker3DAction";
-import { Marker3DType } from "./Marker3DType";
 
 export interface IMarker3D {
-    world_to_local?: ITransform;
-    ns: string;
-    id: number;
-    type: Marker3DType;
-    action: Marker3DAction;
-    pose: ITransform;
-    scale: IVector3;
-    color: IColorRGBA;
-    lifetime: number;
-    frame_id: string;
-    frame_locked: boolean;
-    points: IVector3[];
-    colors: IColorRGBA[];
-    text: string;
-    mesh_resource: string;
-    mesh_use_embedded_materials: boolean;
+  world_to_local?: {
+    position: IVector3;
+    orientation: IQuaternion;
+  };
+  ns: string;
+  id: number;
+  type: number;
+  action: number;
+  pose: {
+    position: IVector3;
+    orientation: IQuaternion;
+  };
+  scale: IVector3;
+  color: IColorRGBA;
+  lifetime: number;
+  frame_id: string;
+  frame_locked: boolean;
+  points: IVector3[];
+  colors: IColorRGBA[];
+  text: string;
+  mesh_resource: string;
+  mesh_use_embedded_materials: boolean;
 }
