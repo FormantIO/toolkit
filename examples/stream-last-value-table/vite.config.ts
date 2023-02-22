003e4ff2ae0c0abaa99adc8fc98745cb33674911
alenjdev <alen@formant.io>
2023-02-22 13:12:10 -0600

    publish demo

--- examples/stream-last-value-table/vite.config.ts
+++ examples/stream-last-value-table/vite.config.ts
@@ -4,6 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/last-known-value/",
   plugins: [react(), tsconfigPaths()],
 });
