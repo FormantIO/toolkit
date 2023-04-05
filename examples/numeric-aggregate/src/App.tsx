40b2236bc9ad7667b5420b9688f7819177b4bb31
alenjdev <alen@formant.io>
2023-04-05 10:36:58 -0500

    publish demo

--- examples/numeric-aggregate/src/App.tsx
+++ examples/numeric-aggregate/src/App.tsx
@@ -4,7 +4,11 @@ import { LoadingIndicator, useFormant, useScrubberTime } from "@formant/ui-sdk";
 import { dummyData } from "./utils/dummyData";
 import { getTypedConfiguration } from "./utils/getTypedConfiguration";
 import { ConfigurationTypes } from "./types";
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+function timeout(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 function App() {
   const context = useFormant();
@@ -14,17 +18,28 @@ function App() {
   );
   const time = useScrubberTime();
 
+  const isConfiguraionReady = (_: any) => _ !== undefined;
+
+  const waitForConfiguration = useCallback(async () => {
+    let configurationReady = false;
+    let tries = 5;
+    while (!configurationReady && tries !== 0) {
+      configurationReady = isConfiguraionReady(configuration);
+      tries -= 1;
+      await timeout(2000);
+    }
+    setLoading(false);
+  }, [configuration]);
+
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
+    waitForConfiguration();
+  }, [configuration]);
 
   const config = useMemo(() => {
     return !!configuration && Object.keys(configuration).length > 3
       ? configuration
       : dummyData;
-  }, [configuration]);
+  }, [configuration, loading]);
 
   return (
     <div className="App">
