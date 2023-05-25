b7f0b96617eb0378bab3cfc162e9ae149c0d43da
J. Holmes <james.holmes@formant.io>
2023-05-25 12:18:41 -0600

    add outputs for browser/bundled script generation

--- README.md
+++ README.md
@@ -173,7 +173,7 @@ Yes, you can do it in two ways, first as an ES6 module:
   import {
     Authentication,
     Fleet,
-  } from "https://cdn.jsdelivr.net/npm/@formant/data-sdk/dist/data-sdk.es6.js";
+  } from "https://cdn.jsdelivr.net/npm/@formant/data-sdk/dist/data-sdk.bundle.es.js";
   await Authentication.waitTilAuthenticated();
   const devices = await Fleet.getDevices();
   window.document.body.innerHTML = devices.map((_) => _.name).join("<br>");
@@ -183,7 +183,7 @@ Yes, you can do it in two ways, first as an ES6 module:
 or as a non-module:
 
 ```html
-<script src="https://cdn.jsdelivr.net/npm/@formant/data-sdk/dist/data-sdk.umd.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@formant/data-sdk/dist/data-sdk.bundle.umd.js"></script>
 <script type="module">
   const { Authentication, Fleet } = window.FormantDataSDK;
   await Authentication.waitTilAuthenticated();
--- packages/data-sdk/vite.config.js
+++ packages/data-sdk/vite.config.js
@@ -1,24 +1,28 @@
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/main.ts"),
-      name: "FormantDataSDK",
-      fileName: (format) => `data-sdk.${format}.js`,
-      formats: ["es", "cjs"],
-    },
-    sourcemap: true,
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [/node_modules/],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {},
+export default defineConfig(({ mode }) => {
+  const isBundle = mode === "bundle";
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/main.ts"),
+        name: "FormantDataSDK",
+        fileName: (format) =>
+          `data-sdk${isBundle ? ".bundle" : ""}.${format}.js`,
+        formats: ["es", isBundle ? "umd" : "cjs"],
+      },
+      sourcemap: !isBundle,
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: isBundle ? [] : [/node_modules/],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {},
+        },
       },
     },
-  },
+  };
 });
