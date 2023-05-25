e1dd94fa84d0f75489d4f2e6de97632a6227e3e5
J. Holmes <james.holmes@formant.io>
2023-05-25 12:03:58 -0600

    cleaning up unneeded polyfills to node

--- packages/data-sdk/vite.config.js
+++ packages/data-sdk/vite.config.js
@@ -1,16 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 export default defineConfig({
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [NodeGlobalsPolyfillPlugin(), NodeModulesPolyfillPlugin()],
-    },
-  },
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
@@ -23,7 +14,6 @@ export default defineConfig({
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: [/node_modules/],
-      plugins: [rollupNodePolyFill(), nodeResolve()],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
