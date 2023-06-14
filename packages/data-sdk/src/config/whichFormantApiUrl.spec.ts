551421241f382dd43a837a46d1d1fddcb54df7c3
J. Holmes <107068664+jholmes-formant@users.noreply.github.com>
2023-06-14 11:05:18 -0600

    MC-90: add support for arbitrary api paths (#97)

---
+++ packages/data-sdk/src/config/whichFormantApiUrl.spec.ts
@@ -0,0 +1,69 @@
+import { describe, it, expect } from "vitest";
+import { whichFormantApiUrl } from "./whichFormantApiUrl";
+
+describe("whichFormantApiUrl()", () => {
+  it("should return the default url", () => {
+    const url = whichFormantApiUrl({}, new URLSearchParams());
+    expect(url).toBe("https://api.formant.io");
+  });
+
+  describe("defined environments", () => {
+    it.each<[string, string]>([
+      ["formant_dev", "https://api-dev.formant.io"],
+      ["formant_stage", "https://api-stage.formant.io"],
+      ["formant_local", "https://api.formant.local"],
+    ])("should", (urlKey, expected) => {
+      const params = new URLSearchParams(`${urlKey}`);
+      const url = whichFormantApiUrl({}, params);
+      expect(url).toBe(expected);
+    });
+  });
+
+  it(`should look up a setting on the "global" object`, () => {
+    const global = { FORMANT_API_URL: "https://custom.formant.local" };
+    const url = whichFormantApiUrl(global, new URLSearchParams());
+    expect(url).toBe("https://custom.formant.local");
+  });
+
+  describe("custom urls", () => {
+    it("should accept custom API urls in query string", () => {
+      const params = new URLSearchParams(
+        `foo&formant_url=${encodeURIComponent(
+          "https://some.other.formant.domain.local"
+        )}&bar&baz`
+      );
+      const url = whichFormantApiUrl({}, params);
+      expect(url).toBe("https://some.other.formant.domain.local");
+    });
+
+    it("should ignore custom API invalid urls", () => {
+      const params = new URLSearchParams(
+        `foo&formant_url=${encodeURIComponent("this-isn't-a-url")}&bar&baz`
+      );
+      const url = whichFormantApiUrl({}, params);
+      expect(url).toBe("https://api.formant.io");
+    });
+  });
+
+  describe("precedence", () => {
+    it(`should use pre-defined environments over custom and global`, () => {
+      const params = new URLSearchParams(
+        `foo&formant_stage&formant_url=wat&baz`
+      );
+      const global = { FORMANT_API_URL: "https://custom.formant.local" };
+      const url = whichFormantApiUrl(global, params);
+      expect(url).toBe("https://api-stage.formant.io");
+    });
+
+    it(`should use custom-defined environments over global`, () => {
+      const params = new URLSearchParams(
+        `foo&formant_url=${encodeURIComponent(
+          "https://some.other.formant.domain.local"
+        )}&bar&baz`
+      );
+      const global = { FORMANT_API_URL: "https://custom.formant.local" };
+      const url = whichFormantApiUrl(global, params);
+      expect(url).toBe("https://some.other.formant.domain.local");
+    });
+  });
+});
---
+++ packages/data-sdk/src/config/whichFormantApiUrl.ts
@@ -0,0 +1,43 @@
+const DEFAULT_FORMANT_API_URL = "https://api.formant.io";
+
+interface IHasString {
+  get(key: string): string | null;
+  has(key: string): boolean;
+}
+
+export function whichFormantApiUrl(global: any, urlParams: IHasString) {
+  if (urlParams.has("formant_stage")) {
+    return "https://api-stage.formant.io";
+  }
+
+  if (urlParams.has("formant_dev")) {
+    return "https://api-dev.formant.io";
+  }
+
+  if (urlParams.has("formant_local")) {
+    return "https://api.formant.local";
+  }
+
+  if (urlParams.has("formant_url")) {
+    const customUrl = urlParams.get("formant_url");
+    if (customUrl !== null) {
+      try {
+        return new URL(customUrl).origin;
+      } catch {
+        console.warn(
+          `Ignoring malformed \`formant_url\` url parameter: ${customUrl}`
+        );
+      }
+    }
+  }
+
+  if (
+    typeof global !== "undefined" &&
+    "FORMANT_API_URL" in global &&
+    typeof global.FORMANT_API_URL === "string"
+  ) {
+    return global.FORMANT_API_URL;
+  }
+
+  return DEFAULT_FORMANT_API_URL;
+}
