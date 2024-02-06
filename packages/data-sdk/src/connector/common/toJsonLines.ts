8902cad7d6e8028479cc95d5cd42ae3cb390d297
ranaya-formant <66638393+ranaya-formant@users.noreply.github.com>
2024-02-05 16:32:12 -0800

    Revert "Merging in universe connector (#150)"
    This reverts commit 586ae9e379da34bc910279b96741056eae612b47.

--- packages/data-sdk/src/connector/common/toJsonLines.ts
+++
@@ -1,3 +0,0 @@
-export function toJsonLines(objects: any[]): string {
-  return objects.reduce((acc, _) => `${acc}${JSON.stringify(_)}\n`, "");
-}
--- packages/data-sdk/src/connector/model/IUniverseOdometry.ts
+++
@@ -1,6 +0,0 @@
-import { IPoseWithCovariance } from "../../model/IPoseWithCovariance";
-import { ITransform } from "../../model/ITransform";
-
-export interface IUniverseOdometry extends IPoseWithCovariance {
-  worldToLocal?: ITransform;
-}
