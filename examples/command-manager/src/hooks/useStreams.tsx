55c861a29100cc6a4e785407cef7f88399ab9bd0
Alen <alen@formant.io>
2024-03-04 17:31:17 -0600

    publish demo

--- examples/command-manager/src/hooks/useStreams.tsx
+++ examples/command-manager/src/hooks/useStreams.tsx
@@ -1,8 +1,8 @@
-import { Fleet } from '@formant/data-sdk';
+import { Fleet, IStream } from '@formant/data-sdk';
 import { useEffect, useState } from 'react';
 
 export const useStreams = (device: any) => {
-  const [streams, setStreams] = useState([]);
+  const [streams, setStreams] = useState<IStream[]>([]);
 
   useEffect(() => {
     if (!device) return;
