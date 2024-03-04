324e6e6d02f1ed3c4bfc936a4d34d33e3591c5e7
Alen <alen@formant.io>
2024-03-04 17:29:35 -0600

    update command manager to use sdk

--- examples/command-manager/src/hooks/useStreams.tsx
+++ examples/command-manager/src/hooks/useStreams.tsx
@@ -1,27 +1,12 @@
-import { Authentication } from "@formant/data-sdk";
-import { useEffect, useState } from "react";
-
-const getStreams = async () => {
-  if (await Authentication.waitTilAuthenticated()) {
-    const response = await fetch("https://api.formant.io/v1/admin/streams", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + Authentication.token,
-      },
-    });
-
-    const streams = await response.json();
-    return streams.items.filter((_: { enabled: boolean }) => _.enabled);
-  }
-};
+import { Fleet } from '@formant/data-sdk';
+import { useEffect, useState } from 'react';
 
 export const useStreams = (device: any) => {
   const [streams, setStreams] = useState([]);
 
   useEffect(() => {
     if (!device) return;
-    getStreams().then((_) => {
+    Fleet.getStreams().then((_) => {
       setStreams(_);
     });
   }, [device]);
