3a525bed2fd6d5f2a3d3c16f06cbca921f59c0a9
andrey mazepas <andrey@formant.io>
2024-03-08 16:01:57 -0300

    each pcd worker with a cache

--- packages/data-sdk/src/connector/data/PcdLoaderWorker.ts
+++ packages/data-sdk/src/connector/data/PcdLoaderWorker.ts
@@ -1,19 +1,35 @@
-import { parse, loadFromBase64 } from "./pcd";
+import { duration } from "../common/duration";
+import { StoreCache } from "./StoreCache";
+import { parse, loadFromBase64, IPcd } from "./pcd";
 
-onmessage = (
+const cache = new StoreCache<string, IPcd>({
+  capacity: 1000,
+  timeout: 1 * duration.minute,
+});
+
+onmessage = async (
   event: MessageEvent<{ id?: number; url?: string; pointCloud: string }>
 ) => {
   const { url, id, pointCloud } = event.data;
-  (async () => {
-    if (url) {
+
+  if (url) {
+    // Check if the point cloud for the given URL is already in the cache
+    const cached = cache.get(url, async () => {
+      // Fetch the point cloud and store it in the cache
       const pcd = parse(
         await fetch(url, { mode: "cors" }).then((r) => r.arrayBuffer())
       );
-      postMessage({ url, pcd });
+      return pcd;
+    });
+
+    if (cached) {
+      postMessage({ url, pcd: cached });
     }
-    if (id && pointCloud) {
-      const pcd = loadFromBase64(pointCloud);
-      postMessage({ id, pcd });
-    }
-  })();
+  }
+
+  if (id && pointCloud) {
+    // Process the base64-encoded point cloud
+    const pcd = loadFromBase64(pointCloud);
+    postMessage({ id, pcd });
+  }
 };
