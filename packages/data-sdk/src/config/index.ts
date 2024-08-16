7480f20a6bb3087635b9b37c08c2083cd76c3fb6
Eric Barch <ericb@ericbarch.com>
2024-08-16 16:11:31 -0400

    Detect env correctly when hosted in the FE (#179)
    Co-authored-by: Eric Barch <eric@formant.io>

--- packages/data-sdk/src/config/index.ts
+++ packages/data-sdk/src/config/index.ts
@@ -6,5 +6,8 @@ export const FORMANT_API_URL = whichFormantApiUrl(
     typeof window !== "undefined" && window.location
       ? window.location.search
       : undefined
-  )
+  ),
+  typeof window !== "undefined" && window.location
+    ? window.location.host
+    : undefined
 );
